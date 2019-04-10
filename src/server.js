fb73ec1818bd3b203908ab5eeb11c24991558bc4
Tadeu Augusto Dutra Pinto <tadeuaugusto@gmail.com>
2019-04-10 18:25:41 -0300

    Includes cors

--- src/server.js
+++ src/server.js
@@ -23,6 +23,7 @@ app.use((req, res) => {
     return next();
 })
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
