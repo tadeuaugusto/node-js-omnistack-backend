0452707be3b1d00ddf0ea1a9dbe3696397cb21d9
Tadeu Augusto Dutra Pinto <tadeuaugusto@gmail.com>
2019-04-10 15:34:11 -0300

    Installs mongoose and configures mongodb atlas

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-g5z0l.mongodb.net/omnistack?retryWrites=true', {
+    useNewUrlParser: true
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
